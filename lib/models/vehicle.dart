e58bd5a598cdfbe609d8a724c3b8f654f8070214
ernanyJ <julioernany@gmail.com>
2024-02-05 22:49:57 -0400

    class vehicle

--- lib/models/vehicle.dart
+++ lib/models/vehicle.dart
@@ -1,1 +1,35 @@
-class Vehicle {}
+import 'package:flutter/material.dart';
+
+class Vehicle {
+  String modelo;
+  Color cor;
+  String placa;
+  String chassi;
+  int anoDeFabricacao;
+  double rendimentokm;
+  String tipoCombustivel;
+  double capacidadeCombustivel;
+  double quilometragem;
+  bool disponivel;
+  bool precisaAbastecer;
+  bool precisaManutencao;
+  bool temSeguro;
+  bool alugado;
+
+  Vehicle({
+    required this.modelo,
+    required this.cor,
+    required this.placa,
+    required this.chassi,
+    required this.anoDeFabricacao,
+    required this.rendimentokm,
+    required this.tipoCombustivel,
+    required this.capacidadeCombustivel,
+    required this.quilometragem,
+    required this.disponivel,
+    required this.precisaAbastecer,
+    required this.precisaManutencao,
+    required this.temSeguro,
+    required this.alugado,
+  });
+}
--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -12,19 +12,8 @@ class CarsPage extends StatelessWidget {
     return Container(
       color: const Color.fromARGB(255, 40, 16, 78),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          Container(
-            margin: const EdgeInsets.only(left: 20, top: 30),
-            child: Text(
-              'Veículos',
-              style: GoogleFonts.lato(
-                color: Colors.white,
-                fontSize: 30,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-          ),
           // CARD THAT DISPLAYS THE LIST OF DRIVERS
           DriversList(screenWidth: screenWidth, screenHeigth: screenHeigth),
         ],
@@ -47,93 +36,61 @@ class DriversList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(right: 270, left: 20),
-      color: const Color.fromARGB(88, 0, 0, 0),
-      width: screenWidth - 300,
-      height: screenHeigth - 73,
-      child: Stack(
-        children: [
-          GridView.count(
-            primary: false,
-            padding: const EdgeInsets.all(20),
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            crossAxisCount: 3,
-            children: <Widget>[
-              Container(
-                width: 20,
-                height: 20,
-                padding: const EdgeInsets.all(8),
-                color: Colors.indigo[100],
-                child: Center(
-                    child: const Text("He'd have you all unravel at the")),
-              ),
-              Container(
-                width: 20,
-                height: 20,
-                padding: const EdgeInsets.all(8),
-                color: Colors.blue[200],
-                child: Center(child: const Text('Heed not the rabble')),
-              ),
-              Container(
-                width: 20,
-                height: 20,
-                padding: const EdgeInsets.all(8),
-                color: Colors.blue[300],
-                child: Center(child: const Text('Sound of screams but the')),
-              ),
-              Container(
-                width: 20,
-                height: 20,
-                padding: const EdgeInsets.all(8),
-                color: Colors.blue[400],
-                child: Center(child: const Text('Who scream')),
-              ),
-              Container(
-                width: 20,
-                height: 20,
-                padding: const EdgeInsets.all(8),
-                color: Colors.blue[500],
-                child: Center(child: const Text('Revolution is coming...')),
-              ),
-              Container(
-                width: 20,
-                height: 20,
-                padding: const EdgeInsets.all(8),
-                color: Colors.blue[600],
-                child: Center(child: const Text('Revolution, they...')),
-              ),
-              Container(
-                width: 20,
-                height: 20,
-                padding: const EdgeInsets.all(8),
-                color: Colors.blue[300],
-                child: Center(child: const Text('Sound of screams but the')),
-              ),
-              Container(
-                width: 20,
-                height: 20,
-                padding: const EdgeInsets.all(8),
-                color: Colors.blue[400],
-                child: Center(child: const Text('Who scream')),
-              ),
-              Container(
-                width: 20,
-                height: 20,
-                padding: const EdgeInsets.all(8),
-                color: Colors.blue[500],
-                child: Center(child: const Text('Revolution is coming...')),
-              ),
-              Container(
-                width: 20,
-                height: 20,
-                padding: const EdgeInsets.all(8),
-                color: Colors.blue[600],
-                child: Center(child: const Text('Revolution, they...')),
-              ),
-            ],
-          )
-        ],
+      decoration: BoxDecoration(
+          color: Color.fromARGB(255, 233, 233, 233),
+          borderRadius: BorderRadius.circular(5)),
+      width: screenWidth - 120,
+      height: screenHeigth * 0.6,
+      child: Container(
+        child: GridView.count(
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 7,
+          childAspectRatio: 2 / 3,
+          padding: EdgeInsets.all(8),
+          children: <Widget>[
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+            CarCard(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CarCard extends StatelessWidget {
+  const CarCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        color: Color.fromARGB(38, 0, 0, 0),
       ),
     );
   }
